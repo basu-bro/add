0400ff2c79f17234d74906d06a0a00cba50b7aa1
localhost-programming <67981073+localhost-programming@users.noreply.github.com>
2020-08-21 18:53:52 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+int a,b,sum;
+printf("[•] Enter the 1st number:- ");
+scanf("%d" , &a);
+printf("\n[•] Enter the 1st number:- ");
+scanf("%d" , &b);
+
+sum=a+b;
+
+printf("\n\n[>] Addition of %d and %d is %d\n",a,b,sum);
+}
